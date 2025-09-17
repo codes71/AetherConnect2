582aa65e78d9dcafc7466f80b569a20a0073c034
codes71 <thuraheinsyh@gmail.com>
2025-09-18 00:57:59 +0530

    Cookie 2

--- services/api-gateway/src/auth/auth.controller.ts
+++ services/api-gateway/src/auth/auth.controller.ts
@@ -46,6 +46,7 @@ export class AuthController {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         sameSite: "none",
+        partitioned: true,
         maxAge: 15 * 60 * 1000, // 15 minutes
         path: "/",
       });
@@ -53,6 +54,7 @@ export class AuthController {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         sameSite: "none",
+        partitioned: true,
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         path: "/",
       });
@@ -81,6 +83,7 @@ export class AuthController {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         sameSite: "none",
+        partitioned: true,
         maxAge: 15 * 60 * 1000, // 15 minutes
         path: "/",
       });
@@ -88,6 +91,7 @@ export class AuthController {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         sameSite: "none",
+        partitioned: true,
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         path: "/",
       });
