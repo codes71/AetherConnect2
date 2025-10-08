297eebf9c6e976121b3ab304e8b8467fb3456570
codes71 <thuraheinsyh@gmail.com>
2025-10-08 17:39:57 +0530

    Gotcha .gitignore and useless shit

---
+++ services/api-gateway/src/types/express.d.ts
@@ -0,0 +1,18 @@
+import "@types/express";
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        userId: string;
+        username: string;
+        email?: string;
+        // Add any other properties your user object might have
+      };
+      cookies: {
+        [key: string]: string;
+      };
+    }
+  }
+}
+
+export {};
